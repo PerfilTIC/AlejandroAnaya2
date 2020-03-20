0dc26ec571e78e09558fcc5ab2496bfec0765740
Jhoan Alejandro Anaya <jaanaya88@misena.edu.co>
2020-03-20 01:41:12 -0500

    recuperados los archivos borrados por el head

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -1,8 +1,9 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
-class Main extends CI_Controller{
-    
+class Main extends CI_Controller
+{
+
     public function __construct()
     {
         header('Access-Control-Allow-Origin: *');
@@ -10,119 +11,183 @@ class Main extends CI_Controller{
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         header("Allow: GET, POST, OPTIONS, PUT, DELETE");
         $method = $_SERVER['REQUEST_METHOD'];
-        if($method == "OPTIONS") {
+        if ($method == "OPTIONS") {
             die();
         };
         parent::__construct();
-        
+
         $this->load->helper('url');
         $this->load->library('session');
+
         $this->load->model('MainModel');
-        
     }
-    public function index(){
-        echo "entra index";
+    public function index()
+    {
+        echo "entra index aaaaaaaaaaaaaaaaa";
     }
-    public function test(){
+    public function test()
+    {
         echo "entra";
     }
-    public function Validate(){
+    public function Validate()
+    {
         $username = $this->input->post('user');
         $pass = $this->input->post('pass');
-		$res = $this->MainModel->login($username,$pass);
-		if($res){
-            $data_res['status']= True;
-            $data_res['id_usuario']= $res->id_usuario;
-			//$data_res['url']= base_url('Dashboard');
-			$data = array(
-				"id"=> $res->id_usuario,
-				"nombre_usuario" =>$res->nombre,
-				"is_loged"=> TRUE
-			);
-			$this->session->set_userdata($data);
-		}else{
-			$data_res['status']= False;
-			$data_res['msg']='Usuario o contraseña incorrectos';
-		}
-		/*
+        $rol = $this->input->post('rol');
+        $res = $this->MainModel->login($username, $pass, $rol);
+        if ($res) {
+            $data_res['status'] = True;
+            $data_res['id_usuario'] = $res->id_usuario;
+            $data_res['rol']= $res->rol;
+            //$data_res['url']= base_url('Dashboard');
+            $data = array(
+                "id" => $res->id_usuario,
+                "nombre_usuario" => $res->nombre,
+                "is_loged" => TRUE
+            );
+            $this->session->set_userdata($data);
+        } else {
+            $data_res['status'] = False;
+            $data_res['msg'] = 'Usuario o contraseña incorrectos';
+        }
+        /*
 		var_dump($res->id_usuario);
 		die;
 		*/
-		echo json_encode($data_res);
-		die;
+        echo json_encode($data_res);
+        die;
     }
-    public function addProducto(){
-        $user = $this->input->post('user');
-        $producto = $this->input->post('producto');
-        $stock = $this->input->post('stock');
-        $precio = $this->input->post('precio');
-        $categoria = $this->input->post('categoria');
+    public function Register(){
+        $usuario = $this->input->post('username');
+        $contraseña = $this->input->post('pass');
+        echo json_encode($this->MainModel->registerUser($usuario,$contraseña));
+    }
+
+    public function addCategoriaPadre()
+    {
+        $nombre = $this->input->post('categoriaP');
+        echo json_encode($this->MainModel->addCategoriaPadre($nombre));
+        die;
+    }
+    public function addCategoria()
+    {
+        if (isset($_FILES["image"]["name"])) {
+
+            $config['upload_path'] = './uploads/';
+            $config['allowed_types'] = 'jpg|png|jpeg';
+            $config['encrypt_name'] = TRUE;
+
+
+            $this->load->library('upload', $config);
+
+            if (!$this->upload->do_upload('image')) {
+                //no sube el archivo
+                //print_r($this->upload->display_errors());
+
+            } else {
+                //si lo sube
+
+                $data = $this->upload->data();
+                $urlFile = base_url() . 'uploads/' . $data["file_name"];
+
+                $nombre = $this->input->post('categoria');
+                $idCatPadre = $this->input->post('id_catPadre');
+
+                echo json_encode($this->MainModel->addCategoria($nombre, $urlFile, $idCatPadre));
+            }
+        }
+        die;
+        /*
+        */
+    }
+    public function addProducto()
+    {
         
-        $this->MainModel->addProducto($user,$producto,$precio,$stock,$categoria);
+        $nombre = $this->input->post('nombre');
+        $precio = $this->input->post('precio');
+        $precioDolar = $this->input->post('precioDolar');
+        $descripcion = $this->input->post('descripcion');
+        $id_categoria = $this->input->post('id_categoria');
+        $resProd = $this->MainModel->addProducto($nombre, $precio, $precioDolar, $descripcion, $id_categoria);
 
-    }
-    public function deleteProducto(){
-        $id_producto = $this->input->post('id_producto');
-        echo json_encode($this->MainModel->deleteProducto($id_producto));
-
-    }
-    public function createUser(){
-        $username = $this->input->post('username');
-        $pass = $this->input->post('pass');
-        if(!$this->MainModel->checkUser($username)){
-            //agregar el usuario
+        
+        //echo json_encode();
+        
+        if ($resProd) {
             
-            if($this->MainModel->createUser($username,$pass)){
-                $res = $this->MainModel->login($username,$pass);
+            if (isset($_FILES["images"]['name'])) {
                 
-                if($res){
 
-                    $response['status'] = true;
-                    $response['id_usuario']=$res->id_usuario;
+                $config['upload_path'] = './uploads/';
+                $config['allowed_types'] = 'jpg|png';
+
+
+                $this->load->library('upload', $config);
+                $file = count($_FILES['images']['name']);
+             
+                $varFile = $_FILES;
+                for ($i = 0; $i < $file; $i++) {
+
+                    $_FILES['images']['name'] = $varFile['images']['name'][$i];
+                    $_FILES['images']['type'] = $varFile['images']['type'][$i];
+                    $_FILES['images']['tmp_name'] = $varFile['images']['tmp_name'][$i];
+                    $_FILES['images']['error'] = $varFile['images']['error'][$i];
+                    $_FILES['images']['size'] = $varFile['images']['size'][$i];
+                    if ($this->upload->do_upload('images')) {
+                        //sube el archivo
+                        //print_r($this->upload->display_errors());
+                        
+
+                        $urlFile = base_url() . 'uploads/' . $varFile['images']['name'][$i];
+                        if($this->MainModel->addImagenProducto($nombre,$urlFile,$i)){
+                            $response = true;
+                        }else{
+                            $response= false;
+                        }
+                    } else{
+                        $response=false;
+                    }
+                   
+                    
                 }
-
-            }else{
-                $response['status'] = false;
-                $response['msg'] = 'error al crear el usuario';
             }
         }else{
-            //msg de respuesta
-            $response['status'] = false;
-            $response['msg'] = "el usuario ya existe";
-            
+            $response=false;
+           
         }
         echo json_encode($response);
     }
-    public function updateProducto(){
+    public function getDetailProducto(){
         $id_producto = $this->input->post('id_producto');
-        $id_categoria =$this->input->post('id_categoria');
-        $nombre =$this->input->post('nombre');
-        $precio =$this->input->post('precio');
-        $stock =$this->input->post('stock');
-        echo json_encode($this->MainModel->updateProducto($id_producto,$id_categoria,$nombre,$precio,$stock));
-
-
-        
-
-    }
-
-    public function listarProductos(){
-        $id_user = $this->input->post('id_usuario');
-        echo json_encode($this->MainModel->listarProductos($id_user));
-
-    }
-    public function addCategoria(){
-        $id_user = $this->input->post('id_user');
-        $categoria = $this->input->post('categoria');
-        $this->MainModel->addCategoria($id_user,$categoria);
+        echo json_encode($this->MainModel->getDetailProducto($id_producto));
         die;
     }
-    public function listarCategorias(){
-        $id_user = $this->input->post('id_user');
-        echo json_encode($this->MainModel->listCategorias($id_user));
+
+    public function listarCategoriasPadre()
+    {
+        echo json_encode($this->MainModel->listarCategoriasPadre());
+        die;
+    }
+    public function listarCategorias()
+    {
+        echo json_encode($this->MainModel->listarCategorias());
+        die;
+    }
+    public function getAllProductos(){
+        echo json_encode($this->MainModel->getAllProductos());
+        die;
+    }
+    public function getProductos(){
+     
+        $id_categoria = $this->input->post('id_categoria');
+      
+        echo json_encode($this->MainModel->getProductos($id_categoria));
+        die;
+    }
+
+    public function getCategorias(){
+        $idCatPadre = $this->input->post('idCatPadre');
+        echo json_encode($this->MainModel->getCategorias($idCatPadre));
         die;
     }
 }
-
-
-?>
--- application/models/MainModel.php
+++ application/models/MainModel.php
@@ -1,146 +1,218 @@
 <?php
-class MainModel extends CI_Model{
-    function __construct(){
+class MainModel extends CI_Model
+{
+    function __construct()
+    {
         $this->load->database();
     }
-    public function login($user,$pass){
-        $data = $this->db->get_where('usuarios',array('nombre'=> $user, 'password'=>$pass),1);
-        if($data->result_array()){
+    public function login($user, $pass, $rol)
+    {
+        $data = $this->db->get_where('usuarios', array('nombre' => $user, 'password' => $pass, 'rol' => $rol), 1);
+        if ($data->result_array()) {
 
             return $data->row();
-        }else{
+        } else {
             return false;
         }
-
     }
-    public function listCategorias($id_user){
-        $data = $this->db->get_where('categorias',array('id_usuario'=> $id_user));
-        if($data->result_array()){
-
-            return $data->result_array();
-        }else{
-            return false;
-        }
-
-    }
-    public function checkUser($user){
-        $data = $this->db->get_where('usuarios',array('nombre'=> $user),1);
-        if($data->result_array()){
-
-            return true;
-        }else{
-            return false;
-        }
-
-    }
-    public function createUser($user,$pass){
-        $arrayInsert = array(
-            'nombre' => $user,
-            'contraseña'=>$pass
-        );
-        $this->db->insert('usuarios', $arrayInsert);
-        if($this->db->affected_rows()>0){
+    public function registerUser($usuario,$contraseña){
+        $data = $this->db->get_where('usuarios',array('nombre'=> $usuario),1);
+       
+        if(!$data->result_array()){
+            $arrayInsert = array(
+                'nombre'=>$usuario,
+                'password'=>$contraseña,
+                'rol'=>'user'
+                
+            );
+            $this->db->insert('usuarios', $arrayInsert);
+            $data2 = $this->db->get_where('usuarios',array('nombre'=> $usuario),1);
+            $data2 = $data2->row();
            
+            $response['status'] = true;
+            $response['id_usuario']= $data2->id_usuario; 
+            $response['rol']=$data2->rol;
             
-
-            return true;
-      
         }else{
-            return false;
-
+            
+            $response['status']=false;
+            $response['msg']= "Usuario no disponible.";
         }
-        
-
+        return $response;
+        die;
 
     }
-    public function updateProducto($id_producto,$id_categoria,$nombre,$precio,$stock){
-        $data = array(
-            'id_categoria' => $id_categoria,
+
+    public function addCategoriaPadre($nombre)
+    {
+        $arrayInsert = array(
+            'nombre' => $nombre
+        );
+        $this->db->insert('categoriaPadre', $arrayInsert);
+
+        if ($this->db->affected_rows() > 0) {
+
+
+
+            $response['status'] = true;
+        } else {
+            $response['status'] = false;
+            $response['msj'] = "error al crear la categoria.";
+        }
+        return $response;
+    }
+    public function addCategoria($nombre, $urlFile, $idCatPadre)
+    {
+        $arrayInsert = array(
             'nombre' => $nombre,
-            'precio' => $precio,
-            'stock' => $stock
-    );
-    
-    $this->db->where('id_producto', $id_producto);
-    $this->db->update('productos', $data);
-    if($this->db->affected_rows()>0){
-        return true;
-    }else{
-        return false;
+            'foto' => $urlFile,
+            'id_categoriaPadre' => $idCatPadre
+        );
+        $this->db->insert('categoria', $arrayInsert);
+
+        if ($this->db->affected_rows() > 0) {
+
+
+
+            $response['status'] = true;
+        } else {
+            $response['status'] = false;
+            $response['msj'] = "error al crear la categoria.";
+        }
+        return $response;
     }
-    }
-    public function listarProductos($id_user){
-        //$data = $this->db->get_where('productos',array('id_usuario'=> $id_user));
-        $sql = "select categorias.nombre_categoria,productos.* 
-        from productos
-        join categorias on productos.id_categoria = categorias.id_categorias
-        where productos.id_usuario ='$id_user'";
-        $data = $this->db->query($sql);
-        if($data->result_array()){
+    public function listarCategoriasPadre()
+    {
+        $data = $this->db->get('categoriaPadre');
+        if ($data->result_array()) {
 
             return $data->result_array();
-        }else{
+        } else {
             return false;
         }
-        
-
     }
-    public function deleteProducto($id_producto){
-        $this->db->where('id_producto', $id_producto);
-        $this->db->delete('productos');
-        if($this->db->affected_rows()>0){
-            return true;
-        }else{
+    public function listarCategorias()
+    {
+        $data = $this->db->get('categoria');
+        if ($data->result_array()) {
+
+            return $data->result_array();
+        } else {
             return false;
         }
-
     }
-
-
-
-    public function addCategoria($user,$categoria){
-        $arrayInsert = array(
-            'id_usuario' => $user,
-            'nombre_categoria'=>$categoria
-        );
-        $this->db->insert('categorias', $arrayInsert);
-        if($this->db->affected_rows()>0){
-           
-            $response= true;
-        }else{
-            $response= false;
-
-        }
-        echo json_encode($response);
-    }
-
-    public function addProducto($user,$nombre,$precio,$stock,$categoria){
+    public function addProducto($nombre, $precio, $precioDolar, $descripcion, $id_categoria)
+    {
         $arrayInsert = array(
             'nombre' => $nombre,
-            'id_categoria' => $categoria,
             'precio' => $precio,
-            'stock'=>$stock,
-            'id_usuario'=>$user
-
-
+            'precioDolar' => $precioDolar,
+            'descripcion' => $descripcion,
+            'id_categoria' => $id_categoria
         );
-        /*
-        $arrayInsertCat = array(
-            'id_usuario' => $user,
-            'nombre_categoria' => 
-        )
-        */
         $this->db->insert('productos', $arrayInsert);
-        if($this->db->affected_rows()>0){
-           
-            $response= true;
-        }else{
-            $response= false;
 
+        if ($this->db->affected_rows() > 0) {
+
+
+
+            return true;
+        } else {
+            return false;
         }
-        echo json_encode($response);
+    }
+    public function addImagenProducto($nombreProducto,$urlFile,$bandera){
+        $data = $this->db->get_where('productos',array('nombre'=> $nombreProducto),1);
+        if($data->result_array()){
+            
+            $data = $data->row();
+            $idProducto = $data->id_producto;
+            switch($bandera){
+                case 0:
+                    $dataUpdate = array(
+                        'url1' => $urlFile,
+                        
+                );
+                
+                    
+            break;
+            case 1:
+                $dataUpdate = array(
+                    'url2' => $urlFile,
+                    
+            );
+            
+                break;
+                case 2:
+                    $dataUpdate = array(
+                        'url3' => $urlFile,
+                        
+                );
+                
+                break;
+            }
+            $this->db->where('id_producto', $idProducto);
+            $this->db->update('productos', $dataUpdate);
+            if ($this->db->affected_rows() > 0) {
+    
+    
+    
+                return true;
+            } else {
+                return false;
+            }
+
+
+        }else{
+            
+            return false;
+        }
+    }
+    public function getAllProductos(){
+        $query = $this->db->get('productos');
+        if ($query->result_array()) {
+
+            return $query->result_array();
+        } else {
+            return false;
+        }
+      
+    }
+    public function getProductos($id_categoria){
+        $this->db->from('productos');
+        $this->db->where('id_categoria',$id_categoria);
+        $query = $this->db->get();
+        if ($query->result_array()) {
+
+            return $query->result_array();
+        } else {
+            return false;
+        }
 
 
     }
-    
+    public function getDetailProducto($idProducto){
+        $data = $this->db->get_where('productos', array('id_producto' => $idProducto), 1);
+        if ($data->result_array()) {
+
+            return $data->row();
+        } else {
+            return false;
+        }
+
+    }
+    public function getCategorias($idCatPadre){
+       
+        $this->db->from('categoria');
+        
+        $this->db->where('id_categoriaPadre',$idCatPadre);
+        $data = $this->db->get();
+        if ($data->result_array()) {
+
+            return $data->result_array();
+        } else {
+            return false;
+        }
+
+    }
 }
